04f890b1b151c137d0f98eadfc45086c5dc3950e
bareckij <bareckijmaksim@gmail.com>
2024-09-21 19:15:21 +0300

    modified

--- Task4/lab0_4.py
+++ Task4/lab0_4.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import time
 start_time = time.perf_counter()
 
@@ -28,3 +29,18 @@ else:
     print("The numbers are too big!")
 finish_time = time.perf_counter()
 print('Time: ' + str(finish_time - start_time))
+=======
+import time
+start_time = time.perf_counter()
+
+fib1 = fib2 = 1
+num = open("input.txt").read()
+n = int(num)
+if 0<n<45:
+    for i in range(2, n):
+        fib1, fib2 = fib2, fib1 + fib2
+    open('output.txt', 'w').write(str(fib2))
+else:   
+    print("The numbers are too big!")
+print(time.perf_counter() - start_time)
+>>>>>>> 5e67e7e (modified)
