1f86e23187209f4b8af5aba28617bed0ec8dc640
bareckij <mgfuf333@gmail.com>
2024-11-04 14:35:44 +0300

    refactor

--- alg_lab2/task5/test/test.py
+++ alg_lab2/task5/test/test.py
@@ -1,19 +1,16 @@
 import time
 import tracemalloc
 from alg_lab2.task5.src.task5 import is_majority_element
- 
+import utils
 tracemalloc.start()
 t_start = time.perf_counter()
   
-with open('alg_lab2/task5/textf/input.txt', 'r') as f:
-    n = int(f.readline().strip())
-    A = list(map(int, f.readline().strip().split()))
 
-result = is_majority_element(A)
+n, arr= utils.read_data_from_file('alg_lab2/task5/textf/input.txt')
 
-with open('alg_lab2/task5/textf/output.txt', 'w') as f:
-    f.write(str(result))
+result = is_majority_element(arr)
 
+utils.write_data_to_file('alg_lab2/task5/textf/output.txt', str(result))
 print('Тест примера')
 print('Время работы: %s секунд' % (time.perf_counter() - t_start))
 print("Память:", tracemalloc.get_traced_memory()[1] / (1024 ** 2), "МБ")
