fd8563130878de3e4bc241aac61273813e1adac2
bareckij <mgfuf333@gmail.com>
2024-11-06 15:11:28 +0300

    refactor

--- alg_lab3/task8/src/task8.py
+++ alg_lab3/task8/src/task8.py
@@ -1,7 +1,4 @@
-import math
-
 def find_k_nearest_points(points, k):
-    # Сортируем точки по расстоянию до начала координат
     points.sort(key=lambda p: p[0]**2 + p[1]**2)
     return points[:k]
 
